5634abb65fc7ddba8eb4da64c0ba6f2d5a521fe1
Tim Park <timfpark@gmail.com>
2020-03-30 13:39:19 -0700

    Cache map tiles locally as we have to fetch them

--- lib/ios/AirMaps/AIRMapLocalTileOverlay.m
+++ lib/ios/AirMaps/AIRMapLocalTileOverlay.m
@@ -13,7 +13,6 @@
 
 @implementation AIRMapLocalTileOverlay
 
-
 -(void)loadTileAtPath:(MKTileOverlayPath)path result:(void (^)(NSData *, NSError *))result {
     NSMutableString *tileFilePath = [self.URLTemplate mutableCopy];
     [tileFilePath replaceOccurrencesOfString: @"{x}" withString:[NSString stringWithFormat:@"%li", (long)path.x] options:0 range:NSMakeRange(0, tileFilePath.length)];
@@ -23,9 +22,23 @@
         NSData* tile = [NSData dataWithContentsOfFile:tileFilePath];
         result(tile,nil);
     } else {
-        result(nil, nil);
+        NSString *tileUrlString = [NSString stringWithFormat:@"https://map.trailguru.app/tile/%ld/%ld/%ld", path.z, path.x, path.y];
+        NSURL *tileUrl = [NSURL URLWithString:tileUrlString];
+        NSURLRequest *request = [NSURLRequest requestWithURL:tileUrl];
+        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            if (result) result(data, connectionError);
+            if (!connectionError) {
+                NSMutableString* tileFileDirectory = [self.URLTemplate mutableCopy];
+                [tileFileDirectory replaceOccurrencesOfString: @"{z}" withString:[NSString stringWithFormat:@"%li", (long)path.z] options:0 range:NSMakeRange(0, tileFileDirectory.length)];
+                [tileFileDirectory replaceOccurrencesOfString: @"{x}" withString:[NSString stringWithFormat:@"%li", (long)path.x] options:0 range:NSMakeRange(0, tileFileDirectory.length)];
+                [tileFileDirectory replaceOccurrencesOfString: @"{y}" withString:@"" options:0 range:NSMakeRange(0, tileFileDirectory.length)];
+                if (![[NSFileManager defaultManager] fileExistsAtPath:tileFileDirectory])
+                    [[NSFileManager defaultManager] createDirectoryAtPath:tileFileDirectory withIntermediateDirectories:YES attributes:nil error:nil];
+
+                [[NSFileManager defaultManager] createFileAtPath:tileFilePath contents:data attributes:nil];
+            }
+        }];
     }
 }
-
 
 @end
